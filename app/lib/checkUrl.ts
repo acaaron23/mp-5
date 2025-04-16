093a6acb8a974ad62e0c504bf657f07ff9fff77e
Aaron Chen <acaaron@bu.edu>
2025-04-15 23:54:17 -0400

    Changes

--- app/lib/checkUrl.ts
+++ app/lib/checkUrl.ts
@@ -2,7 +2,7 @@
 
 export default async function checkURL(url: string): Promise<boolean>{
     try{
-        const res = await fetch(url)
+        const res = await fetch(url);
         return res.status >= 200 && res.status < 400;
     }
     catch(err){
